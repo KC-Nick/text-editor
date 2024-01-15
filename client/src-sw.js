da6de888f70c71075f3e14342a4bcc6863418160
KC-Nick <wilson.kasinichole@gmail.com>
2024-01-15 15:57:50 -0500

    added baseline code for sw file, db file, and webpack config, along with editing package.json

--- client/src-sw.js
+++ client/src-sw.js
@@ -5,26 +5,44 @@ const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
+//caching and routing files for webpack build
 precacheAndRoute(self.__WB_MANIFEST);
 
+//servers cached responses to reqs or fetches from network
 const pageCache = new CacheFirst({
   cacheName: 'page-cache',
   plugins: [
+    //handles cacheable responses
     new CacheableResponsePlugin({
       statuses: [0, 200],
     }),
+    //sets expiration
     new ExpirationPlugin({
       maxAgeSeconds: 30 * 24 * 60 * 60,
     }),
   ],
 });
 
+//alternative to precaching for specific routes
 warmStrategyCache({
   urls: ['/index.html', '/'],
   strategy: pageCache,
 });
 
+//nav reqs
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+registerRoute(
+    // filters requests to cache
+    ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+    new offlineFallback({
+      // cache storage
+      cacheName: 'asset-cache',
+      plugins: [
+        // caches responses to headers for up to 30 days
+        new CacheableResponsePlugin({
+          statuses: [0, 200],
+        }),
+      ],
+    })
+);
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,80 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
+
+//initiates the database
   openDB('jate', 1, {
+
     upgrade(db) {
+      //checks if database already exists
+
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
         return;
       }
+      
+      //creates database with auto id
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// takes content and adds it to the database
+export const putDb = async (content) => {
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  try {
+
+    console.log('PUT to the database');
+
+    // connects to database and version needed
+    const jateDb = await openDB('jate', 1);
+
+    const tx = jateDb.transaction('jate', 'readwrite');
+
+    const store = tx.objectStore('jate');
+
+    const request = store.put({ id: id, content: content });
+    const result = await request;
+    console.log('Data saved to jateDb', result);
+
+  } catch (error) {
+
+    console.log('Error', error);
+    throw error;
+
+  }
+};
+
+// get all content
+export const getDb = async () => {
+
+  try {
+
+    console.log('GET from the database');
+
+    // connects to database and version needed
+    const jateDb = await openDB('jate', 1);
+
+    // create a transaction with a named database and data permissions
+    const tx = jateDb.transaction('jate', 'readonly');
+
+    // open up the object store for desired database
+    const store = tx.objectStore('jate');
+
+    // create a request to get all data in object store
+    const request = store.getAll();
+
+    // waits for the results to the request and logs them, then returns
+    const result = await request;
+    console.log('result.value', result);
+    return result;
+
+  } catch (error) {
+
+    console.log('Error', error);
+    throw error;
+
+  }
+};
 
 initdb();
